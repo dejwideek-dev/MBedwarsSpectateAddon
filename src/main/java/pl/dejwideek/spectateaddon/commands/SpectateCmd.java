0c487b1b7dd4b2c148d2cb293f98f86f218550b6
Harsh <ps4042927@gmail.com>
2023-09-17 20:18:48 +0530

    small typo "a" instead of "as"

--- src/main/java/pl/dejwideek/spectateaddon/commands/SpectateCmd.java
+++ src/main/java/pl/dejwideek/spectateaddon/commands/SpectateCmd.java
@@ -82,8 +82,8 @@ public class SpectateCmd extends BaseCommand {
 
                                 if (status.equals(ArenaStatus.RUNNING)) {
                                     player.sendMessage(colorApi.process(teleportedMsg
-                                            .replaceAll("%player%", target == null ? "":target.getName())
-                                            .replaceAll("%arena%", as == null? "": as.getName())));
+                                            .replaceAll("%player%", target.getName())
+                                            .replaceAll("%arena%",  as.getName())));
                                     a.addSpectator(p);
                                 }
                                 if (status.equals(ArenaStatus.END_LOBBY)) {
